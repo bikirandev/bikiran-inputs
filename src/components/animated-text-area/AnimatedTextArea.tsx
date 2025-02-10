cc78c9cc2890066462339df07c9cff7d3f778cee
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2025-02-10 10:02:47 +0600

    text area added

---
+++ src/components/animated-text-area/AnimatedTextArea.tsx
@@ -0,0 +1,81 @@
+import { FC, useEffect, useRef, useState } from "react";
+import { cn } from "../../lib/utils/cn";
+import { TAnimateInputField } from "../../lib/types/InputType";
+
+// InputField component with TS types
+const AnimatedTextArea: FC<TAnimateInputField> = (props) => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  const {
+    label,
+    name,
+    formData,
+    className = "",
+    disabled = false,
+    onChange,
+  } = props;
+
+  const handleFocus = () => {
+    if (!disabled && ref.current) {
+      ref.current.focus();
+      setFocused(true);
+    }
+  };
+
+  const handleBlur = () => {
+    if (ref.current) {
+      setFocused(false);
+    }
+  };
+
+  // Focus the input when `focused` is set to true
+  useEffect(() => {
+    if (focused && ref.current) {
+      ref.current.focus();
+    }
+  }, [focused]);
+
+  const isValue =
+    typeof formData?.[name] === "number"
+      ? formData?.[name] > 0
+      : formData?.[name]?.length > 0;
+
+  return (
+    <div className="animate-input">
+      <div
+        className={cn("w-full h-24 relative overflow-visible", className)}
+        onClick={() => setFocused(true)}
+      >
+        <label
+          className={cn(
+            "text-sm font-medium text-primary-700 bg-white absolute top-[15px] left-2.5 -translate-y-1/2 transition-all duration-300",
+            {
+              "-top-2 left-4 translate-x-0 translate-y-0":
+                (focused || isValue) && !disabled,
+              "text-secondary-700": focused,
+            }
+          )}
+        >
+          {label || "Type something"}
+        </label>
+        <textarea
+          ref={ref}
+          name={name}
+          value={formData[name] || ""}
+          onChange={onChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          disabled={disabled}
+          className={cn(
+            "block w-full h-full p-2.5 caret-white border rounded-[8px] text-base outline-none disabled:grayscale transition-colors",
+            {
+              "border-secondary-700 caret-current": focused,
+            }
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+export default AnimatedTextArea;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export {default as AnimatedInputField} from "./components/animated-input-field/A
 export {default as CalculationInputField} from "./components/calculation-input-field/CalculationInputField"
 export {default as DomainInputField} from "./components/domain-input-field/DomainInputField"
 export {default as EmailInputField} from "./components/email-input-field/EmailInputField"
+export {default as AnimatedTextArea} from "./components/animated-text-area/AnimatedTextArea"
