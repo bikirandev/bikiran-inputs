948bba05108d76842cf657a1dee89d13cde417eb
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2025-02-24 10:09:43 +0600

    Bump version to 1.23.2 and refactor InputField component layout

--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -18,26 +18,26 @@ const InputField: FC<TInputField> = ({
   inputClassName = "",
 }) => {
   return (
-    <div>
-      <label className="text-base font-medium text-primary">{label}</label>
-      {required && <span className="text-error opacity-75">*</span>}
-      <div className={cn("w-full h-[45px] relative", className)}>
-        <input
-          type={type}
-          name={name}
-          value={formData[name] || ""}
-          onChange={onChange}
-          onBlur={onBlur}
-          required={required}
-          autoComplete={autoComplete}
-          placeholder={placeholder}
-          disabled={disabled}
-          className={cn(
-            "block w-full h-full px-2.5 mt-1 border rounded-[8px] text-base outline-none disabled:grayscale ",
-            inputClassName
-          )}
-        />
+    <div className={cn("w-full h-[45px] relative", className)}>
+      <div>
+        <label className="text-base font-medium text-primary">{label}</label>
+        {required && <span className="text-error opacity-75">*</span>}
       </div>
+      <input
+        type={type}
+        name={name}
+        value={formData[name] || ""}
+        onChange={onChange}
+        onBlur={onBlur}
+        required={required}
+        autoComplete={autoComplete}
+        placeholder={placeholder}
+        disabled={disabled}
+        className={cn(
+          "block w-full h-full px-2.5 mt-1 border rounded-[8px] text-base outline-none disabled:grayscale ",
+          inputClassName
+        )}
+      />
     </div>
   );
 };
